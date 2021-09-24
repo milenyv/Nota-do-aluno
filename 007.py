399a1db2a5ecab7c41e01fc004496f0cb73274d7
mileny <82714273+milenyv@users.noreply.github.com>
2021-09-24 12:07:27 -0300

    Add files via upload

---
+++ 007.py
@@ -0,0 +1,4 @@
+n1 = float(input("primeira nota do aluno: "))
+n2 = float(input("segunda nota do aluno: "))
+média = (n1 + (n2*2)) / 3
+print("A média entre {:.1f} e {:.1f} é igual a {:.1f}".format(n1, n2, média))
